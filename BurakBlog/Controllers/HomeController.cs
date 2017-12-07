f6fdf7376ed48bc5644c171b30e3d1616103f067
burakkilic44 <32898628+burakkilic44@users.noreply.github.com>
2017-12-07 23:41:37 +0300

    Importing project

---
+++ BurakBlog/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurakBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Iletisim")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [Route("Hakkimizda")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
